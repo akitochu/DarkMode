6a030fd3f940b43548b14193964e3fef86292bb0
DESKTOP-DJK1QF7\akito <akitochu@gmail.com>
2022-12-29 16:22:02 +0000

    working darkmode

--- popup.js
+++ popup.js
@@ -1,17 +1,34 @@
-const button = document.querySelector(".button");
-const circle = document.querySelector(".circle");
+if (document.querySelector(".popup")){
+    const button = document.querySelector(".button");
+    const circle = document.querySelector(".circle");    
 
+    let buttonOn = false;   
+    
+    
 
-let buttonOn = false;   
+    button.addEventListener("click", ()=> {
 
-button.addEventListener("click", ()=> {
-    if (!buttonOn){
-        buttonOn = true;
-        circle.style.animation = "moveCircleRight 1s forwards"
-        button.style.animation = "changeBackgroundGreen 1s forwards"
-    }else{
-        buttonOn = false
-        circle.style.animation = "moveCircleLeft 1s forwards"
-        button.style.animation = "changeBackgroundRed 1s forwards"
-    }
-})
+        if (!buttonOn){
+            buttonOn = true;
+            circle.style.animation = "moveCircleRight 1s forwards"
+            button.style.animation = "changeBackgroundGreen 1s forwards"
+            chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id, allFrames: true},
+                    files: ["appOn.js"]
+                })
+            })
+        }else{
+            buttonOn = false;
+            circle.style.animation = "moveCircleLeft 1s forwards"
+            button.style.animation = "changeBackgroundRed 1s forwards"
+            chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id, allFrames: true},
+                    files: ["appOff.js"]
+                })
+            })
+        }
+    })
+}
+ 
